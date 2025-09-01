8ba3327a2fdf7bb53c367dfc5649500acc944c10
abdulghanii <abdulghani7827@gmail.com>
2025-08-31 21:44:09 -0700

    Initial commit LaraNext CRUD with Laravel + Next.js

---
+++ backend/config/cors.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'paths' => ['api/*'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'], // Your Next.js app URL
+    'allowed_headers' => ['*'],
+
+];
+
